d1f763c5a96946af06c052c86b7e642233662006
pinnnnn <dddcc35@gmail.com>
2020-08-11 16:16:27 +0800

    First commit the exited code.

---
+++ eda.py
@@ -0,0 +1,169 @@
+import pandas as pd
+
+mydat = pd.read_csv('~/Junior_Python/Placement_Data_Full_Class.csv')
+print(type(mydat))
+
+# 維度
+print(mydat.shape)
+print(len(mydat))
+cols = (mydat.columns)
+print(cols.tolist())
+
+
+# types of columns
+import numpy as np
+
+is_numeric = mydat.dtypes != 'object'
+numeric_cols = mydat.columns[is_numeric]
+
+
+# missing values
+missing_cols = mydat.columns[np.where(mydat.isna())[1]]
+print(np.unique(missing_cols))
+print(missing_cols.value_counts() / len(mydat))
+### salary missing deeper explore
+not_placed = np.where(mydat['status']=='Not Placed')[0]
+salary_missing = np.where(mydat['salary'].isna())[0]
+
+print((not_placed == salary_missing).all())
+
+
+
+# numeric distribution
+### distribution
+pd.set_option("display.max_columns", 15)
+print(mydat.describe(include=[np.number]))
+
+### outlier detect
+def inRangeCheck(x, left, right): # x: series, left: int, right: int; outlier: list
+    #outlier = [x[i] for i in range(len(x)) if x[i]<left] + [x[i] for i in range(len(x)) if x[i]>right]
+    outlier = pd.concat([x[x<left], x[x>right]]).sort_values()
+    return outlier
+
+def outlierDetect(x): # x: series; outlier: list
+    x_series = pd.Series(data=x)
+    q1 = x_series.quantile(.25)
+    q3 = x_series.quantile(.75)
+    iqr = q3 - q1
+    outlier = pd.concat([x_series[x_series<(q1-1.5*iqr)], x_series[x_series>(q1+1.5*iqr)]]).sort_values()
+    return outlier
+
+
+cols = [nc for nc in numeric_cols if nc not in ['sl_no','salary']]
+out_range = mydat.loc[:,cols].apply(inRangeCheck, left=0, right=100)
+out_salary = inRangeCheck(mydat.loc[:,'salary'], left=0,right=float('inf'))
+print('Out of range: ', out_range, '\n')
+print('Negative salary: ', out_salary, '\n')
+
+cols = [nc for nc in numeric_cols if nc not in ['sl_no']]
+outlier_normal = []
+for col in cols:
+    temp = outlierDetect(mydat.loc[:, col])
+    outlier_normal.append(temp)
+
+outlier_len = {}
+for i in range(len(outlier_normal)):
+    outlier_len.update({outlier_normal[i].name :len(outlier_normal[i])})
+
+print("outlier_len: ", outlier_len, '\n\n')
+
+### correlation
+corr = mydat.loc[:,numeric_cols].corrwith(mydat['salary'])
+print(corr)
+
+
+# categorical distribution
+categorical_cols = mydat.columns[mydat.dtypes == 'object']
+print(type(categorical_cols))
+
+frequency = {}
+i = 0
+for cc in categorical_cols:
+    if i == 0:
+        print('Count Values: \n')
+    temp = mydat[cc].value_counts()
+    frequency.update({cc: temp})
+    print(temp)
+    i += 1
+
+
+
+# Visualization
+import matplotlib.pyplot as plt
+import numpy as np
+import seaborn as sns
+
+### Single variable distribution check
+##### Categorical Variables
+n_row = 4
+n_col = 2
+fig, axes = plt.subplots(nrows=n_row, ncols=n_col, figsize=(10,7))
+plt.subplots_adjust(hspace=0.7, wspace=0.3)
+count=0
+for i in range(n_row):
+    for j in range(n_col):
+            sns.barplot(x=frequency[categorical_cols[count]].values, y=frequency[categorical_cols[count]].index,
+                ax=axes[i,j])
+            axes[i,j].set_title(categorical_cols[count], fontsize=15)
+            count+=1
+
+#plt.savefig('/home/finepin/Junior_Python/categorical_eda1.png')
+
+
+### Which variable is associated with the "status" variable
+##### Numeric variables
+plot_numeric_cols = [nc for nc in numeric_cols if nc not in ['sl_no']]
+print(plot_numeric_cols)
+
+fig, axes = plt.subplots(nrows=len(plot_numeric_cols), ncols=1, figsize=(7,10))
+for i in range(len(plot_numeric_cols)):
+    if(plot_numeric_cols[i]!='salary'):
+        sns.violinplot(data=mydat, x=plot_numeric_cols[i], y='status', #hue='status',
+            cut=0, order=['Placed','Not Placed'], scale='count', bw=.3, orient='h',
+            ax=axes[i]) # 指定畫在哪個subplots
+    else:
+        sns.violinplot(data=mydat, x=plot_numeric_cols[i], y='status', hue='status',
+            cut=0, order=['Placed','Not Placed'], scale='count', bw=.3, orient='h',
+            ax=axes[i])
+    axes[i].set_ylabel(plot_numeric_cols[i], rotation=0, fontsize=15, labelpad=27) # ax.set_ylabel
+    axes[i].set_yticks(ticks=[])
+    axes[i].set_xlabel('')
+
+axes[0].set_title('EDA of Numeric Variables', fontsize=20)
+
+#plt.savefig('/home/finepin/Junior_Python/numeric_eda.png')
+
+##### catgorical variables
+plot_categorical_cols = categorical_cols[categorical_cols != 'status']
+#cross_dat = pd.crosstab(index=mydat['status'], columns=[mydat['workex']])
+
+n_row = 4
+n_col = 2
+fix, axes = plt.subplots(nrows=n_row, ncols=n_col, figsize=(10,7))
+plt.subplots_adjust(hspace=0.7, wspace=0.3)
+for i in range(n_row):
+    end = False
+    for j in range(n_col):
+        ind = i*2+j
+        if ind>(len(plot_categorical_cols)-1):
+            end = True
+            break
+        col = plot_categorical_cols[ind]
+        count_table = mydat.groupby(['status',col]).size().reset_index(name='counts')
+        count_table['total'] = count_table['counts'].groupby(count_table[col]).transform('sum')
+        count_table['proportion'] = (count_table['counts']/count_table['total']*100).round(2)
+
+        sns.barplot(data=count_table, x=col, y="proportion", hue="status", ax=axes[i,j])
+        ys = count_table['proportion'].groupby(count_table[col]).max()
+        values = count_table.loc[:,[col,'total']].drop_duplicates(subset=col).set_index(col)['total']
+
+        for x, y, value in zip(range(len(ys)), ys.values, values.values):
+            axes[i,j].text(x=x, y=y, s=str(value), fontsize=12, horizontalalignment='center')
+
+        axes[i,j].set_title(col, fontsize=14)
+        axes[i,j].set_ylabel('')
+        axes[i,j].set_xlabel('')
+
+    if end:
+        break
+#plt.show()
---
+++ logistic_model.py
@@ -0,0 +1,108 @@
+import pandas as pd
+import numpy as np
+import math
+import sys
+
+mydat = pd.read_csv('~/Junior_Python/Placement_Data_Full_Class.csv')
+
+# Build Model
+### Logistic Regression
+from sklearn import linear_model
+from sklearn import model_selection
+import sklearn.metrics as sk_metrics
+import copy
+
+##### Data Preparing
+categorical_predictor = ['gender', 'specialisation']
+numeric_predictor = ['ssc_p', 'hsc_p', 'degree_p']
+response = 'status'
+X_dummy = pd.get_dummies(mydat.loc[:, categorical_predictor], drop_first=True)
+X = pd.concat([X_dummy, mydat.loc[:, numeric_predictor]], axis=1)
+Y = mydat.loc[:, response]
+#Y = mydat.loc[:, response].map({'Placed':0, 'Not Placed':1})
+train_X, test_X, train_Y, test_Y = model_selection.train_test_split(X, Y, test_size=0.2, random_state=101)
+
+##### Select the Best Hyperparameter C
+scorings = ['accuracy','neg_log_loss','f1','roc_auc', sk_metrics.make_scorer(sk_metrics.recall_score, pos_label=-1)]
+scoring_name = copy.deepcopy(scorings)
+scoring_name[4] = 'specificity'
+metrics = []
+metrics_builtin = []
+for i, scoring in enumerate(scorings):
+    print('Now i:', i,'\n')
+
+    LR_model = linear_model.LogisticRegressionCV(cv=5, Cs=12, random_state=101, scoring=scoring, class_weight='Balanced').fit(train_X, train_Y)
+
+    print('CV OK!\n')
+    for value in LR_model.scores_.values():
+        scores = value
+    def myMean(x):
+        return np.mean(x)
+    mean_score = np.apply_along_axis(func1d=myMean, axis=0, arr=scores)
+
+    best_hypara_id = np.where(LR_model.Cs_ == LR_model.C_)
+    #print(mean_score[best_hypara_id])
+    #print([round(ma,4) for ma in sorted(mean_score, reverse=True)])
+    #for coef_path in LR_model.coefs_paths_.values():
+    #    coefs_paths = coef_path
+
+    ##### Fit the model
+    def modelMetric(predict_table, event_name):
+        # cross: a crosstable with the index being whether the predict is correct or not, and the columns being the labels
+        # event_name: the name of  an occuring event
+        cross = pd.crosstab(index=predict_table['correct'], columns=predict_table['label'])
+        columns = cross.columns.tolist()
+        non_event_name = [col for col  in columns if col not in event_name]
+
+        TP = cross.loc['correct', event_name].values[0]
+        FP = cross.loc['wrong', non_event_name].values[0]
+        TN = cross.loc['correct', non_event_name].values[0]
+        FN = cross.loc['wrong', event_name].values[0]
+
+        precision = (TP / (TP+FP)).round(3)
+        sensitivity = (TP / (FN+TP)).round(3)
+        specificity = (TN / (TN+FP)).round(3)
+        accuracy = ((TP+TN) / (TP+TN+FP+FN)).round(3)
+
+        return pd.Series({"precision":precision, "sensitivity":sensitivity, "specificity":specificity, "accuracy":accuracy})
+
+    ####### predict and sort out
+    predict_Y = LR_model.predict(test_X)
+    predict_table = pd.DataFrame({
+        "label": test_Y ,
+        "predict": predict_Y})
+
+    predict_table["correct"] = (predict_table['label'] == predict_table['predict']).map({True:'correct', False:'wrong'})
+    #print(predict_table['correct'])
+
+    event_name = ['Placed']
+    #event_name = [1]
+    metric = pd.DataFrame({"value":modelMetric(predict_table, event_name), 'scoring':scoring_name[i]})
+    metric = metric.reset_index()
+    metrics.append(metric)
+
+    #print('scoring_name:', scoring_name)
+    #print(LR_model.coef_)
+    #print(LR_model.intercept_)
+
+
+metrics = pd.concat(metrics, axis=0)
+#metrics.to_csv('/home/finepin/Junior_Python/LR_metrics_balanced.csv', index=False)
+
+print(metrics)
+print(X.columns)
+print(LR_model.coef_)
+print(LR_model.intercept_)
+
+# logit(p(Placed)) = -20.684 + 1.022*Male - 0.255*HR + 0.169*ssc_p + 0.069*hsc_p + 0.082*degree_p
+# Calculate the log odds ratio and the probability
+def LRInterpret(gender_M=0, specialisation_HR=0, ssc_p=50, hsc_p=50, degree_p=50):
+    if any([per >100 for per in [ssc_p, hsc_p, degree_p]]):
+        sys.exit("Errors: The grades are presented by percentage, which are limited from 0 to 100")
+    logit = np.array([gender_M, specialisation_HR, ssc_p, hsc_p, degree_p]).dot(np.array(LR_model.coef_[0])) + LR_model.intercept_
+    odds_ratio = math.exp(logit)
+    probability = 1/(1+math.exp(-logit))
+    return {'logit': logit, 'odds_ratio': odds_ratio, 'probability': probability}
+
+print(LRInterpret(gender_M=1, ssc_p=60, degree_p=99))
+print(LRInterpret(gender_M=0, ssc_p=101, degree_p=99))
